9a7482571d9c19a5d270daeecd998e5fd5f83606
Jannes Dailidow <jannes@dailidow.de>
2025-10-01 22:33:12 +0200

    updated config loader for flexibility

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,25 +4,21 @@ import (
 	"os"
 	"strings"
 
+	"github.com/goccy/go-yaml"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-type Config struct {
-	Database struct {
-		Type string `mapstructure:"type"`
-		Dsn  string `mapstructure:"dsn"`
-	} `mapstructure:"database"`
-	Language string `mapstructure:"language"`
+func configPath(system bool) (string, error) {
+	if system {
+		// TODO make os aware
+		return "/etc/keymaster", nil
+	} else {
+		return os.UserConfigDir()
+	}
 }
 
-var Defauls = map[string]any{
-	"database.type": "sqlite",
-	"database.dsn":  "./keymaster.db",
-	"language":      "en",
-}
-
-func LoadConfig[T any](cmd *cobra.Command, defaults map[string]any) (T, error) {
+func LoadConfig[T any](cmd *cobra.Command, defaults map[string]any, additional_config_file_path *string) (T, error) {
 	var c T
 	v := viper.New()
 
@@ -34,6 +30,9 @@ func LoadConfig[T any](cmd *cobra.Command, defaults map[string]any) (T, error) {
 	// files (first file found wins)
 	v.SetConfigName("keymaster")
 	v.SetConfigType("yaml")
+	if additional_config_file_path != nil {
+		v.AddConfigPath(*additional_config_file_path)
+	}
 	if home, err := os.UserHomeDir(); err == nil {
 		v.AddConfigPath(home + "/.config")
 	}
@@ -41,14 +40,15 @@ func LoadConfig[T any](cmd *cobra.Command, defaults map[string]any) (T, error) {
 
 	// env
 	v.AutomaticEnv()
+	v.AllowEmptyEnv(true)
 	v.SetEnvPrefix("keymaster")
 	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
 	// cli
-	// TODO: maybe needs to trigger additional parsing beferohand
+	// TODO maybe needs to trigger additional parsing beferohand (most likely nots)
 	v.BindPFlags(cmd.Flags())
 
-	// TODO: maybe not needed
+	// TODO maybe not needed
 	// if err := v.ReadInConfig(); err != nil {
 	// 	if _, ok := err.(v.ConfigFileNotFoundError); !ok {
 	// 		return nil, err
@@ -61,4 +61,26 @@ func LoadConfig[T any](cmd *cobra.Command, defaults map[string]any) (T, error) {
 	}
 
 	return c, nil
+}
+
+func WriteConfigFile[T any](c *T, system bool) error {
+	path, err := configPath(system)
+	if err != nil {
+		return err
+	}
+
+	data, err := yaml.Marshal(c)
+	if err != nil {
+		return err
+	}
+
+	// TODO recursively create directory if not present
+
+	// TODO review permissions, because database secrets may be saved here in user mode (user restricted read permissions when not in system mode)
+	err = os.WriteFile(path, data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
