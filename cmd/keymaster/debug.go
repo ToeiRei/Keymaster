50b5b874bf32202b42b6f8f34c80e4487a60926b
Viktoria Rei Bauer <vbauer@stargazer.at>
2025-12-30 16:46:29 +0100

    cmd/keymaster: add debug command (debugCmd) for runtime diagnostics

---
+++ cmd/keymaster/debug.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+var debugCmd = &cobra.Command{
+	Use:   "debug",
+	Short: "Dump debug information about config, env, flags and settings",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("--- KEYMASTER DEBUG ---")
+		// Config file used
+		used := viper.ConfigFileUsed()
+		fmt.Printf("Config file used: %s\n", used)
+
+		// Viper settings
+		settings := viper.AllSettings()
+		b, err := json.MarshalIndent(settings, "", "  ")
+		if err != nil {
+			log.Printf("could not marshal viper settings: %v", err)
+		} else {
+			fmt.Println("-- viper.AllSettings() --")
+			fmt.Println(string(b))
+		}
+
+		// Flags
+		fmt.Println("-- flags --")
+		cmd.Flags().VisitAll(func(f *pflag.Flag) {
+			val := f.Value.String()
+			fmt.Printf("%s = %s\n", f.Name, val)
+		})
+
+		// Environment variables of interest
+		fmt.Println("-- environment (KEYMASTER_*, KEYMASTER, CONFIG*) --")
+		for _, e := range os.Environ() {
+			if strings.HasPrefix(e, "KEYMASTER_") || strings.HasPrefix(e, "KEYMASTER") || strings.HasPrefix(e, "CONFIG") {
+				fmt.Println(e)
+			}
+		}
+
+		// Print GO env hints
+		fmt.Printf("PWD=%s\n", os.Getenv("PWD"))
+		fmt.Println("--- END DEBUG ---")
+	},
+}
