4e3b61e9f1613b4933c67b34664a3cfa83c8db26
ToeiRei <vbauer@stargazer.at>
2026-01-05 16:01:11 +0100

    add license info

--- internal/core/validation_test.go
+++ internal/core/validation_test.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 ToeiRei
+// Keymaster - SSH key management system
+// This source code is licensed under the MIT license found in the LICENSE file.
+
 package core
 
 import "testing"
