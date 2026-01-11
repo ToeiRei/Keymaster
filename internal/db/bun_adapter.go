8ef02d6824a54ecb7b13b063134190baea8b1abc
Jannes Dailidow <jannes@dailidow.de>
2026-01-11 15:13:45 +0100

    fixed tag matcher parser and refactored naming to seperate tags from tag matchers

--- internal/db/bun_adapter.go
+++ internal/db/bun_adapter.go
@@ -215,7 +215,7 @@ func getMultipleAccountsBun(ctx context.Context, bdb *bun.DB, opts ...func(*bun.
 }
 
 func GetAccountsByTagBun(ctx context.Context, bdb *bun.DB, tag string) ([]model.Account, error) {
-	tag_qb, err := tags.GetTagQueryBuilder(tag)
+	tag_qb, err := tags.QueryBuilderFromTagMatcher(tag)
 	if err != nil {
 		return nil, err
 	}
