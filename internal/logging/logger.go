ab590aac2cb73cdbeec18f118fead9ec26c7ec76
ToeiRei <vbauer@stargazer.at>
2026-01-15 19:01:39 +0100

    refactor: remove duplicate copyright notices in logger.go

--- internal/logging/logger.go
+++ internal/logging/logger.go
@@ -2,6 +2,10 @@
 // Keymaster - SSH key management system
 // This source code is licensed under the MIT license found in the LICENSE file.
 
+// Copyright (c) 2026 Keymaster Team
+// Keymaster - SSH key management system
+// This source code is licensed under the MIT license found in the LICENSE file.
+
 package logging
 
 import (
