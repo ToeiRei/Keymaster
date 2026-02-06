59ab168f2660821a4f8fab86742288f62500d130
Jannes Dailidow <jannes@dailidow.de>
2026-02-06 22:40:48 +0100

    added comment spacing after copyright header

--- buildvars/vars.go
+++ buildvars/vars.go
@@ -1,6 +1,7 @@
 // Copyright (c) 2026 Keymaster Team
 // Keymaster - SSH key management system
 // This source code is licensed under the MIT license found in the LICENSE file.
+
 // Package buildvars contains variables injected at build time.
 // Package buildvars contains variables injected at build time.
 package buildvars
