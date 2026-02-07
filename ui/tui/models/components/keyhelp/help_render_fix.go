15395277ad7746ac5b6bfa7d615bda38490476f5
Jannes Dailidow <jannes@dailidow.de>
2026-02-07 21:00:17 +0100

    upheld package comment convention and removed duplicate init calling

--- ui/tui/models/components/keyhelp/help_render_fix.go
+++ ui/tui/models/components/keyhelp/help_render_fix.go
@@ -1,6 +1,7 @@
 // Copyright (c) 2026 Keymaster Team
 // Keymaster - SSH key management system
 // This source code is licensed under the MIT license found in the LICENSE file.
+
 // Package keyhelp contains helper views used to render key help in the TUI.
 package keyhelp
 
--- ui/tui/models/components/popup/msg.go
+++ ui/tui/models/components/popup/msg.go
@@ -1,6 +1,7 @@
 // Copyright (c) 2026 Keymaster Team
 // Keymaster - SSH key management system
 // This source code is licensed under the MIT license found in the LICENSE file.
+
 // Package popup provides popup components and helpers for the TUI.
 package popup
 
