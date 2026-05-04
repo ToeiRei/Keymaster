bf2452205b179876e9e3864c1412a5c67a855aa8
Jannes Dailidow <jannes@dailidow.de>
2026-05-04 16:27:56 +0200

    addd a 3rd tags package witch features a more readable bun

---
+++ tags3/expr.go
@@ -0,0 +1,142 @@
+// Copyright (c) 2026 Keymaster Team
+// Keymaster - SSH key management system
+// This source code is licensed under the MIT license found in the LICENSE file.
+package tags3
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/toeirei/keymaster/util/slicest"
+	"github.com/uptrace/bun"
+)
+
+const (
+	exprAnd         rune   = '&'
+	exprOr          rune   = '|'
+	exprNot         string = "!"
+	exprBracesOpen  rune   = '('
+	exprBracesClose rune   = ')'
+	exprWildcard    string = "*"
+	exprWildcards   string = "**"
+)
+
+type Expr interface {
+	fmt.Stringer
+	Eval(tags Tags) bool
+	Optimize() Expr
+	applyToBunQuery(qb bun.QueryBuilder, column string, mode bunMode) bun.QueryBuilder
+}
+
+type ValueExpr struct{ Value string }
+type AndExpr struct{ Exprs []Expr }
+type OrExpr struct{ Exprs []Expr }
+type NotExpr struct{ Expr Expr }
+
+// [ValueExpr] implements [Expr]
+// [AndExpr] implements [Expr]
+// [OrExpr] implements [Expr]
+// [NotExpr] implements [Expr]
+
+var _ Expr = ValueExpr{}
+var _ Expr = AndExpr{}
+var _ Expr = OrExpr{}
+var _ Expr = NotExpr{}
+
+// var _ Expr = BracesExpr{}
+
+// --- [fmt.Stringer] implementations ---
+
+func (e ValueExpr) String() string {
+	return e.Value
+}
+func (e AndExpr) String() string {
+	return strings.Join(slicest.Map(e.Exprs, func(e Expr) string {
+		switch e.(type) {
+		case OrExpr:
+			return string(exprBracesOpen) + e.String() + string(exprBracesClose)
+		default:
+			return e.String()
+		}
+	}), " "+string(exprAnd)+" ")
+}
+func (e OrExpr) String() string {
+	return strings.Join(slicest.Map(e.Exprs, func(e Expr) string {
+		switch e.(type) {
+		case AndExpr, OrExpr:
+			return string(exprBracesOpen) + e.String() + string(exprBracesClose)
+		default:
+			return e.String()
+		}
+	}), " "+string(exprOr)+" ")
+}
+func (e NotExpr) String() string {
+	switch e.Expr.(type) {
+	case AndExpr, OrExpr:
+		return exprNot + string(exprBracesOpen) + e.Expr.String() + string(exprBracesClose)
+	default:
+		return exprNot + e.Expr.String()
+	}
+}
+
+// --- [Expr.Eval] implementations ---
+
+func (e ValueExpr) Eval(tags Tags) bool {
+	expr := regexp.QuoteMeta(e.Value)
+	expr = strings.ReplaceAll(expr, regexp.QuoteMeta(exprWildcards), ".*")
+	expr = strings.ReplaceAll(expr, regexp.QuoteMeta(exprWildcard), ".")
+	regexpr := regexp.MustCompile("^" + expr + "$")
+	return slicest.Contains(tags, func(tag Tag) bool {
+		return regexpr.MatchString(string(tag))
+	})
+}
+func (e AndExpr) Eval(tags Tags) bool {
+	return !slicest.Contains(e.Exprs, func(expr Expr) bool { return !expr.Eval(tags) })
+}
+func (e OrExpr) Eval(tags Tags) bool {
+	return slicest.Contains(e.Exprs, func(expr Expr) bool { return expr.Eval(tags) })
+}
+func (e NotExpr) Eval(tags Tags) bool {
+	return !e.Expr.Eval(tags)
+}
+
+// --- [Expr.Optimize] implementations ---
+
+func (e ValueExpr) Optimize() Expr { return e }
+func (e AndExpr) Optimize() Expr {
+	e.Exprs = slicest.Map(e.Exprs, func(expr Expr) Expr { return expr.Optimize() })
+
+	// flatten nested and expressions
+	e.Exprs = slicest.Flatten(slicest.Map(e.Exprs, func(expr Expr) []Expr {
+		if expr, ok := expr.(AndExpr); ok {
+			return expr.Exprs
+		}
+		return []Expr{expr}
+	}))
+
+	return e
+}
+func (e OrExpr) Optimize() Expr {
+	e.Exprs = slicest.Map(e.Exprs, func(expr Expr) Expr { return expr.Optimize() })
+
+	// flatten nested or expressions
+	e.Exprs = slicest.Flatten(slicest.Map(e.Exprs, func(expr Expr) []Expr {
+		if expr, ok := expr.(OrExpr); ok {
+			return expr.Exprs
+		}
+		return []Expr{expr}
+	}))
+
+	return e
+}
+func (e NotExpr) Optimize() Expr {
+	e.Expr = e.Expr.Optimize()
+
+	// remove double negation
+	if expr, ok := e.Expr.(NotExpr); ok {
+		return expr.Expr
+	}
+
+	return e
+}
---
+++ tags3/parser.go
@@ -0,0 +1,94 @@
+// Copyright (c) 2026 Keymaster Team
+// Keymaster - SSH key management system
+// This source code is licensed under the MIT license found in the LICENSE file.
+package tags3
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/toeirei/keymaster/util/slicest"
+)
+
+var tagValidationRegexpr = regexp.MustCompile(`^[a-zA-Z0-9_\-+*/.:~=]+$`)
+
+func ParseMatcher(matcher string) (Expr, error) {
+	return parseMatcher(matcher, matcher, 0)
+}
+
+func parseMatcher(matcher string, originalMatcher string, pos int) (Expr, error) {
+	matcherPrev := matcher
+	matcher = strings.TrimSpace(matcher)
+
+	// add removed whitespace to position
+	if parts := strings.SplitN(matcherPrev, matcher, 2); len(parts) > 0 {
+		pos += len(parts[0])
+	}
+
+	// and
+	if parts := splitOnTopLevelChar(matcher, exprAnd); len(parts) > 1 {
+		exprs, err := slicest.MapX(parts, func(part string) (Expr, error) {
+			expr, err := parseMatcher(part, originalMatcher, pos)
+			pos += len(part) + 1
+			return expr, err
+		})
+		return AndExpr{exprs}, err
+	}
+
+	// or
+	if parts := splitOnTopLevelChar(matcher, exprOr); len(parts) > 1 {
+		exprs, err := slicest.MapX(parts, func(part string) (Expr, error) {
+			expr, err := parseMatcher(part, originalMatcher, pos)
+			pos += len(part) + 1
+			return expr, err
+		})
+		return OrExpr{exprs}, err
+	}
+
+	// negation
+	if matcher, negated := strings.CutPrefix(matcher, exprNot); negated {
+		expr, err := parseMatcher(matcher, originalMatcher, pos+len(exprNot))
+		return NotExpr{expr}, err
+	}
+
+	// braces
+	if strings.HasPrefix(matcher, string(exprBracesOpen)) && strings.HasSuffix(matcher, string(exprBracesClose)) {
+		matcher = matcher[1 : len(matcher)-1]
+		return parseMatcher(matcher, originalMatcher, pos+1)
+	}
+
+	// raw value
+	if tagValidationRegexpr.MatchString(matcher) {
+		return ValueExpr{matcher}, nil
+	}
+
+	// invalid matcher string
+	posFrom, posTo := pos+1, pos+len(matcher)
+	if len(matcher) == 0 {
+		return nil, fmt.Errorf("invalid tag %q in matcher %q at position %d", matcher, originalMatcher, posFrom)
+	}
+	return nil, fmt.Errorf("invalid tag %q in matcher %q at position %d-%d", matcher, originalMatcher, posFrom, posTo)
+}
+
+func splitOnTopLevelChar(expr string, char rune) []string {
+	var result []string
+	var depth int
+	var cursor int
+
+	for i, ch := range expr {
+		switch ch {
+		case exprBracesOpen:
+			depth++
+		case exprBracesClose:
+			depth--
+		case char:
+			if depth <= 0 {
+				result = append(result, expr[cursor:i])
+				cursor = i + 1
+			}
+		}
+	}
+
+	return append(result, expr[cursor:])
+}
