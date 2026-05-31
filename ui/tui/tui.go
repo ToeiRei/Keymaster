bbef26035fc97a580b7a52e5feca68bbfaf6fee5
Jannes Dailidow <jannes@dailidow.de>
2026-05-31 14:03:50 +0200

    refactor: minor change to test data creation

--- ui/tui/tui.go
+++ ui/tui/tui.go
@@ -18,11 +18,11 @@ func Run() error {
 	ct := testui.NewClient()
 
 	// test accounts
-	_, _ = ct.CreateAccount(context.Background(), "root", "1.2.3.4", 22, "ssh", "password123")
-	_, _ = ct.CreateAccount(context.Background(), "user", "1.2.3.4", 22, "ssh", "password123")
-	_, _ = ct.CreateAccount(context.Background(), "srv", "10.0.0.1", 22, "ssh", "password123")
-	_, _ = ct.CreateAccount(context.Background(), "mark", "1.2.3.4", 22, "ssh", "password123")
-	_, _ = ct.CreateAccount(context.Background(), "admin", "10.20.0.1", 222, "cisco", "password123")
+	_a1, _ := ct.CreateAccount(context.Background(), "root", "1.2.3.4", 22, "ssh", "password123")
+	_a2, _ := ct.CreateAccount(context.Background(), "user", "1.2.3.4", 22, "ssh", "password123")
+	_a3, _ := ct.CreateAccount(context.Background(), "srv", "10.0.0.1", 22, "ssh", "password123")
+	_a4, _ := ct.CreateAccount(context.Background(), "mark", "1.2.3.4", 22, "ssh", "password123")
+	_a5, _ := ct.CreateAccount(context.Background(), "admin", "10.20.0.1", 222, "cisco", "password123")
 	// test publicKeys
 	_, _ = ct.CreatePublicKey(context.Background(), "Sha-your-mom ashtdjhk-fbaskjdfhal_sdvkhaösdljhask-zdpjwb", "my-key", tags.Tags{"user:jannes", "company:work", "server-ci"})
 	_, _ = ct.CreatePublicKey(context.Background(), "Sha-your-mom ashtdjhk-fbaskjdfhal_sdvkhaösdljhask-öutyfb", "my-key", tags.Tags{"user:jannes", "company:none"})
@@ -31,12 +31,12 @@ func Run() error {
 	_, _ = ct.CreatePublicKey(context.Background(), "Sha-420 asdjhk-fbaskjterhl_sdvkhaghdjfdljhask-ödhfb", "421", tags.Tags{"user:toeirei", "company:none"})
 	_, _ = ct.CreatePublicKey(context.Background(), "Sha-69 asdjkhk-fbdfhtdftrhhal_sdvkhaösu656zsk-ödjhtfb", "69", tags.Tags{"user:somebodyelse", "company:evilgoogle", "server-ci"})
 	// test links
-	_, _ = ct.CreateLink(context.Background(), 1, "(user:jannes | user:toeirei) & !company:work", time.Now().Add(time.Hour))
-	_, _ = ct.CreateLink(context.Background(), 2, "!user:somebodyelse", time.Now().Add(time.Hour))
-	_, _ = ct.CreateLink(context.Background(), 3, "server-ci", time.Now().Add(time.Hour))
-	_, _ = ct.CreateLink(context.Background(), 4, "company:evilgoogle", time.Now().Add(time.Hour))
-	_, _ = ct.CreateLink(context.Background(), 5, "company:work", time.Now().Add(time.Hour))
-	_, _ = ct.CreateLink(context.Background(), 5, "company:big_money", time.Now())
+	_, _ = ct.CreateLink(context.Background(), _a1.Id, "(user:jannes | user:toeirei) & !company:work", time.Now().Add(time.Hour))
+	_, _ = ct.CreateLink(context.Background(), _a2.Id, "!user:somebodyelse", time.Now().Add(time.Hour))
+	_, _ = ct.CreateLink(context.Background(), _a3.Id, "server-ci", time.Now().Add(time.Hour))
+	_, _ = ct.CreateLink(context.Background(), _a4.Id, "company:evilgoogle", time.Now().Add(time.Hour))
+	_, _ = ct.CreateLink(context.Background(), _a5.Id, "company:work", time.Now().Add(time.Hour))
+	_, _ = ct.CreateLink(context.Background(), _a5.Id, "company:big_money", time.Now())
 
 	// add delay "middleware"
 	cm := mock.NewClient(mock.WitchBaseClient(ct), mock.WitchPre(func(method string, args map[string]any) error {
