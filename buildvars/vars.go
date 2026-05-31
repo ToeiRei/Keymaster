3aba757a3da7b59fbbd1571ed8bdea3a8aa22c5a
Jannes Dailidow <jannes@dailidow.de>
2026-05-31 11:19:15 +0200

    improved buildvars defaults

--- buildvars/vars.go
+++ buildvars/vars.go
@@ -5,5 +5,41 @@
 // Package buildvars contains variables injected at build time.
 package buildvars
 
+import (
+	"runtime/debug"
+)
+
 // Version is set at link time via `-ldflags -X github.com/toeirei/keymaster/buildvars.Version=...`.
-var Version string = "unknown version"
+var Version string
+
+func init() {
+	if Version == "" {
+		Version = "unknown"
+
+		if buildInfo, ok := debug.ReadBuildInfo(); ok {
+			var vcs, vcsRevision, vcsModified string
+			for _, setting := range buildInfo.Settings {
+				switch setting.Key {
+				case "vcs":
+					vcs = setting.Value
+				case "vcs.revision":
+					vcsRevision = setting.Value
+				case "vcs.modified":
+					vcsModified = setting.Value
+				}
+			}
+
+			if vcs != "" && vcsRevision != "" {
+				if vcs == "git" {
+					vcsRevision = vcsRevision[:7]
+				}
+
+				Version = vcs + ":" + vcsRevision
+
+				if vcsModified == "true" {
+					Version += "*"
+				}
+			}
+		}
+	}
+}
